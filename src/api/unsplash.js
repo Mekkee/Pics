892853a1ae1782f7762838e73f80cf2fd2bd9af2
Mekke <mekke@live.se>
2020-08-05 17:11:19 +0200

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID 1cc0e22fe44a9a0cd964d7b56e18af68ec61568e90d5c8e3d2f0e772986d67c8'
+        Authorization: 'Client-ID xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
     }
 });
